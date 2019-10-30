d4e04605c8f4e64135e9306dfdfed5c9d0b2c954
Surface Pro 6 <s.hadzhipopov@hotmail.com>
2019-10-30 12:32:02 +0200

    minor update in comment

--- Classes/ITimeBuilder.cs
+++ Classes/ITimeBuilder.cs
@@ -8,9 +8,11 @@ namespace BerlinClock.Classes
 {
     //just as an idea to use builder pattern, because we can build the parts of time independently of one another       
     //the clock converter does not check for nulls, when building (ToString())
-    //we can safely remove the interface, only if every parse task was big, the we might need more clear separation   
+    //we can safely remove the interface, only if every parse task was much bigger
+    //the we might need more clear separation (or if not all parts of the time were necessary)  
+    //just as a time enthusiast, you can get part of the time ;)
     public interface ITimeBuilder 
-    {
+    {        
         void BuildMinutes(int minutes);
         void BuildSeconds(int minutes);
         void BuildHours(int minutes);
